acfcf9e8c057df105003041ecf4ff812c4201a27
Jez humble <jez@jezhumble.net>
2012-09-06 00:21:47 +0000

    Added reference for Chef config and install nginx

--- cookbooks/wordpress/recipes/default.rb
+++ cookbooks/wordpress/recipes/default.rb
@@ -1,3 +1,17 @@
+# This installs all the necessary bits and pieces to run the continuousdelivery.com site
+# It assumes you're running Ubuntu 12.04 LTS "Precise"
+# You should create a user with sudo rights before running this, as it turns off the ability to ssh in as root
+# Based on http://wordpress.stackexchange.com/questions/2490/what-is-the-best-caching-option-for-wordpress-multi-site-on-non-shared-hosting
+# Copyright 2012 Jez Humble
+# Licensed under the BSD 2-line license
+
+package "nginx"
+
+service "nginx" do
+  supports "default" => [ :restart, :reload, :status ];
+  action [ :enable, :start ]
+end
+
 service "ssh" do
   service_name "ssh"
   supports "default" => [ :restart, :reload, :status ];
